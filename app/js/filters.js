20503cf75b93db46a93487a562dcdd0bc057f137
Caleb Pollman <cpollman1@gmail.com>
2016-04-05 16:40:57 -0600

    comittals

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,42 +1,52 @@
-//convert snake_case to kebab-case
-app.filter('kebab', function () {
-  return function(input){
-    return input
-  }
-});
+// //convert snake_case to kebab-case
+// app.filter('kebab', function () {
+//   return function(input){
+//     return input.split('_').join('-')
+//   }
+// });
 
 
-//Returns an acronym
-app.filter('acronym', function() {
-  return function(input){
-    return input
-  }
-});
+// //Returns an acronym
+// app.filter('acronym', function() {
+//   return function(input){
+//     return input.match(/\b(\w)/g).join('');
+//   }
+// });
 
 
-//Alternates capitialization of string
-app.filter('aLtCaPs', function() {
-  return function(input){
-    return input
-  }
-});
+// //Alternates capitialization of string(did camecase instead-caleb)
+// app.filter('aLtCaPs', function() {
+//   return function(input){
+//     return input.replace(/^([A-Z])|[\s-_](\w)/g, function(match, p1, p2, offset) {
+//         if (p2) return p2.toUpperCase();
+//         return p1.toLowerCase();        
+//     });
+//   }
+// });
 
 
 //Use with ng-repeat and filter the collection
-app.filter('startsWithA', function () {
+app.filter('startsWithA', function() {
   return function(input) {
-    return input
+    var friends = [];
+    for (var i =0; i < input.length; i ++) {
+      if (input[i].name[0] === 'A') {
+        friends.push(input[i]);
+        return friends;
+      }  
+    }
   }
 });
 
 //Use with ng-repeat and an input to search a collection for string match
 app.filter('strMatch', function () {
-  return function (input, search) {
-    return input
+  return function (input, letter) {
+    var searchResult = [];
+    for (var i = 0; i < input.length; i++) {
+      if (input[i].name === letter) {
+        searchResult.push(input[i]);
+        return searchResult;
+      }
+    }
   }
-});
-
-//Come up with on your own
-app.filter('?????', function() {
-  //Do Cool Things...
 });
